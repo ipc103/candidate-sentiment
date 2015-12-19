004aacca0e8b82be867177d151f92f1221ef52ca
ipc103 <iancandysemail@gmail.com>
2015-12-19 17:11:23 -0500

    remove nil values

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -6,7 +6,12 @@ class CandidatesController < ApplicationController
 
   def data
     @candidate = Candidate.find(params[:id])
-    sentiments = @candidate.data_points.map { |data_point| {createdAt: data_point.created_at.strftime('%y-%m-%d'), probability: data_point.tweeted_about_sentiment} }
+    sentiments = @candidate.data_points.map do |data_point|
+      if data_point.tweeted_about_sentiment 
+        { createdAt: data_point.created_at.strftime('%y-%m-%d'), 
+          probability: data_point.tweeted_about_sentiment } 
+      end
+    end.flatten
     render json: sentiments
   end
 end
